56a6954a6baf4ba98f908a5c4a3f063b080398bb
buiquangcong <buicong908@gmail.com>
2026-03-13 14:26:37 +0700

    feat: Add user list page with an Ant Design table to display user data.

--- src/Page/List.tsx
+++ src/Page/List.tsx
@@ -10,10 +10,6 @@ const List: React.FC = () => {
         { key: '4', id: "4", name: 'Nguyễn Văn C', age: "21", major: "Lập trình web-FE", avatar: "https://i.pravatar.cc/150?u=4", status: "active" },
         { key: '5', id: "5", name: 'Nguyễn Văn D', age: "18", major: "Ngôn Ngữ Trung", avatar: "https://i.pravatar.cc/150?u=5", status: "inactive" },
         { key: '6', id: "6", name: 'Nguyễn Văn E', age: "21", major: "Ngôn Ngữ Hàn", avatar: "https://i.pravatar.cc/150?u=6", status: "active" },
-        { key: '7', id: "7", name: 'Nguyễn Văn F', age: "19", major: "Lập trình web-BE", avatar: "https://i.pravatar.cc/150?u=7", status: "active" },
-        { key: '8', id: "8", name: 'Nguyễn Văn G', age: "20", major: "Thiết kế đồ họa", avatar: "https://i.pravatar.cc/150?u=8", status: "inactive" },
-        { key: '9', id: "9", name: 'Nguyễn Văn H', age: "18", major: "Sale", avatar: "https://i.pravatar.cc/150?u=9", status: "active" },
-        { key: '10', id: "10", name: 'Nguyễn Văn I', age: "20", major: "Marketing", avatar: "https://i.pravatar.cc/150?u=10", status: "active" },
     ];
 
     const columns = [
@@ -48,7 +44,7 @@ const List: React.FC = () => {
             <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: 16 }}>
                 <strong style={{ fontSize: '18px' }}>Danh sách người dùng</strong>
             </div>
-            <Table dataSource={data} columns={columns} pagination={{ pageSize: 5 }} />
+            <Table dataSource={data} columns={columns} pagination={{ pageSize: 3 }} />
         </div>
     );
 };
